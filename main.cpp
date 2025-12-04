58c6eb5fe462b0619563a2f207d1d4e4851ef65d
Tommy Coleman <WS421474@weston.ac.uk>
2025-12-04 11:54:11 +0000

    makemove function

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <windows.h>
+#include <thread>
+#include <chrono>
 
 std::vector<std::vector<char>> connectFourBoard = {
-        {' ', 'O', ' ', ' ', ' ', ' ', ' '},
-        {' ', 'X', ' ', ' ', 'O', 'O', ' '},
-        {' ', ' ', 'O', ' ', ' ', 'O', ' '},
-        {'O', ' ', ' ', 'X', ' ', ' ', ' '},
-        {' ', 'X', ' ', ' ', 'X', ' ', ' '},
+        {'X', ' ', 'O', ' ', ' ', 'O', ' '},
+        {' ', 'X', ' ', ' ', 'X', 'X', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' '}
     };
 
@@ -25,11 +28,46 @@ void printBoard(std::vector<std::vector<char>> board) {
         }
         std::cout << "\n";
     }
-    std::cout << "---------------" ;
+    std::cout << "---------------\n" ;
+}
+
+void makeMove(int column, bool isPlayer1, std::vector<std::vector<char>>& board) {
+    
+    for (int i = 0; i < 6; i++) {
+        std::cout << "\x1b[2J\033[1A";
+        if (i > 0) {
+            if (isPlayer1) {
+                board[i-1][column] = ' ';
+                board[i][column] = 'X';
+            } else {
+                board[i-1][column] = ' ';
+                board[i][column] = 'O';
+            }
+            printBoard(board);
+            Sleep(500);
+            // std::cout << "\x1b[2J\033[1A";
+        } else {
+            if (isPlayer1) {
+                board[i][column] = 'X';
+            } else {
+                board[i][column] = 'O';
+            }
+            printBoard(board);
+            Sleep(500);
+            // Clear terminal
+            //system("cls");
+        }
+    }
 }
 
 
-
 int main() {
+    makeMove(0, true, connectFourBoard);
     printBoard(connectFourBoard);
+    // std::cout.write(&connectFourBoard[0][0], connectFourBoard.size());
+    // std::cout.write(&connectFourBoard[0][1], connectFourBoard.size());
+    // std::cout << "Jello, world!" << std::endl;
+    // std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    // std::cout << "\033[1A";  // Move cursor up one line
+    // std::cout << "Hello, world!" << std::endl;
 }
