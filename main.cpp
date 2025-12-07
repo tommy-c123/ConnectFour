0cc9e03d3bb6867f5b9e782f941615cfb31e436a
Tommy <ws421474@weston.ac.uk>
2025-12-07 13:23:30 +0000

    map for amount filled in columns

--- main.cpp
+++ main.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <vector>
-#include <thread>
-#include <chrono>
+#include <unordered_map>
 
 std::vector<std::vector<char>> connectFourBoard = {
-        {'X', ' ', 'O', ' ', ' ', 'O', ' '},
+        {' ', ' ', 'O', ' ', ' ', 'O', ' '},
         {' ', 'X', ' ', ' ', 'X', 'X', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' '}
+        {'X', ' ', ' ', ' ', ' ', ' ', ' '}
     };
 
-void printBoard(std::vector<std::vector<char>> board) {
+void printBoard(const std::vector<std::vector<char>>& board) {
     std::cout << " 1 2 3 4 5 6 7 \n" ;
-    for (std::vector<char> row : board) {
+    for (const std::vector<char>& row : board) {
         std::cout << "|";
-        for (char cell : row) {
+        for (const char cell : row) {
             if (cell == 'X') {
                 std::cout << "\x1b[31m" << cell << "\x1b[0m|";
             } else if (cell == 'O') {
@@ -30,8 +29,7 @@ void printBoard(std::vector<std::vector<char>> board) {
     std::cout << "---------------\n" ;
 }
 
-void makeMove(int column, bool isPlayer1, std::vector<std::vector<char>>& board) {
-    
+void makeMove(const int column, const bool isPlayer1, std::vector<std::vector<char>>& board, int amountFilled) {
     for (int i = 0; i < 6; i++) {
         std::cout << "\x1b[2J\033[1A";
         if (i > 0) {
@@ -42,29 +40,34 @@ void makeMove(int column, bool isPlayer1, std::vector<std::vector<char>>& board)
                 board[i-1][column] = ' ';
                 board[i][column] = 'O';
             }
-            printBoard(board);
-            // std::cout << "\x1b[2J\033[1A";
         } else {
             if (isPlayer1) {
                 board[i][column] = 'X';
             } else {
                 board[i][column] = 'O';
             }
-            printBoard(board);
-            // Clear terminal
-            //system("cls");
         }
     }
+
 }
 
 
 int main() {
-    makeMove(0, true, connectFourBoard);
+    std::unordered_map<int, int> colsFilled = {
+        { 0, 0 },
+        { 1, 0 },
+        { 2, 0 },
+        { 3, 0 },
+        { 4, 0 },
+        { 5, 0 },
+        { 6, 0 },
+    };
+    makeMove(0, true, connectFourBoard, colsFilled[0]);
     printBoard(connectFourBoard);
     // std::cout.write(&connectFourBoard[0][0], connectFourBoard.size());
     // std::cout.write(&connectFourBoard[0][1], connectFourBoard.size());
     // std::cout << "Jello, world!" << std::endl;
-    // std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    // std::cout << "\033[1A";  // Move cursor up one line
+    // std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    // // std::cout << "\033[1A";  // Move cursor up one line
     // std::cout << "Hello, world!" << std::endl;
 }
